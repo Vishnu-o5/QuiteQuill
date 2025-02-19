e0979424742ad42e6944d08fcfe25eb7b39c35be
Vishnu-o5 <vishnusaras0@gmail.com>
2025-02-19 17:03:47 +0530

    branch1 initial commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,7 +3,9 @@ import React,{useState} from "react";
 const Login = () =>{
     return(
         <div className="w-screen md:w-screen md:h-screen bg-gradient-to-r from-red-300 via-orange-100 border-t-rose-100">
-            <div>Hello</div>
+            <form>
+                <div className=""></div>
+            </form>
         </div>
     )    
 }
